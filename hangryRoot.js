d898cbfe9a28e99d5c7683a9cdad22e5e8cbd52b
Nicholas Taylor <t@Ylornico.la>
2023-09-27 10:26:32 -0400

    Added auto-dismiss for overlay

--- hangryRoot.js
+++ hangryRoot.js
@@ -1,3 +1,14 @@
+const theHost = window.location.hostname;
+const theUrl = window.location.href;
+const hungryRootHosts = [
+    'hungryroot.com',
+    'www.hungryroot.com'
+]
+const dashUrls = [
+    'https://hungryroot.com/myhungryroot/',
+    'https://www.hungryroot.com/myhungryroot/',
+];
+
 const getAllCards = () => {
     const allArticles = document.querySelector('div [data-cy="displayed-cards"]').querySelectorAll('article');
     const allCards = [];
@@ -25,5 +36,20 @@ const filterOff = () => {
     for (let card in allCards){
         let theCard = allCards[card];
         theCard.style.display = 'initial';
+    }
+}
+
+const dismissOnboardOverlay = () => {
+    if (dashUrls.indexOf(theUrl) !== -1){
+        const onboardOver = document.querySelector('div[data-cy="106-onboarding-overlay"]');
+        if (onboardOver){
+            onboardOver.querySelector('button[data-cy="106-onboarding-overlay-dismiss"]').click()
+        }
+    }
+}
+
+const hangryRootMain = () => {
+    if (hungryRootHosts.indexOf(theHost) !== -1){
+        dismissOnboardOverlay();
     }
 }
